03348e1ab4ec5fd943d1adabb2fb5c673b672182
Glen0805 <cavitaglenn22131@gmail.com>
2022-10-26 14:30:29 +0800

    added style in layout\

--- src/components/content/time-tracker/TimeTrackerComponent.js
+++ src/components/content/time-tracker/TimeTrackerComponent.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Card } from 'antd';
 
 const TimeTrackerComponent = () => {
     return (
-        <div>TimeTrackerComponent</div>
+
+        <Card>
+
+        </Card>
+
     )
 }
 
