f71a8c3961a7df730fb3427a2f16c54f0cb585bf
Glen0805 <cavitaglenn22131@gmail.com>
2022-11-03 13:39:30 +0800

    added axios

--- client/src/components/content/time-tracker/TimeTrackerComponent.js
+++ client/src/components/content/time-tracker/TimeTrackerComponent.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Card, Divider, Form } from 'antd';
 import SelectTaskComponent from './time-tracker-component/SelectTaskComponent';
 import ProfileComponent from './profile-component/ProfileComponent';
 import TImerComponent from './time-tracker-component/TImerComponent';
 import TimeLogsComponent from './time-logs-component/TimeLogsComponent';
+import axios from 'axios'
 
 const TimeTrackerComponent = () => {
+    const testFunction = () => {
+        axios.get('http://127.0.0.1:3333/home').then((response) => {
+            console.log(response.data)
+        })
+    }
+    useEffect(() => {
+        axios.get('http://127.0.0.1:3333/home').then((response) => {
+            console.log(response.data)
+        })
+
+    }, [])
+
+
     return (
         <div style={{ display: 'flex', flexDirection: 'column', gap: '40px' }}>
             <div style={{ display: 'flex', gap: '20px', justifyContent: 'center' }}>
@@ -13,7 +27,7 @@ const TimeTrackerComponent = () => {
                     <ProfileComponent />
                 </Card>
                 <Card style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
-                    <TImerComponent />
+                    <TImerComponent test={testFunction} />
                     <Divider />
                     <Form
                         layout="vertical">
