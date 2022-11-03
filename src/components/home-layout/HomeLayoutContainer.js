f1d8b10f037d6480d3a37e64891c34354b6a6b30
Glen0805 <cavitaglenn22131@gmail.com>
2022-11-03 12:06:46 +0800

    added lazy loading in routes

--- src/components/home-layout/HomeLayoutContainer.js
+++ src/components/home-layout/HomeLayoutContainer.js
@@ -1,9 +1,10 @@
 import { Breadcrumb, Layout, Menu } from 'antd';
-import React, { useState } from 'react';
+import React, { useState, Suspense } from 'react';
 import menu from './menu';
 import { Route, Routes, Link, Navigate } from 'react-router-dom'
 import contentRoute from './content-routes';
 import './layout.css'
+import { LoadingOutlined } from '@ant-design/icons'
 
 
 const { Header, Content, Footer } = Layout;
@@ -59,18 +60,20 @@ const HomeLayoutContainer = () => {
                         minHeight: 380,
                     }}
                 >
-                    <Routes>
-                        {contentRoute.map((content) =>
-                            content.element ? (<Route
-                                path={content.path}
-                                element={content.element}
-                                key={content.id}
-                                index={content.index} />
-                            ) : null
-                        )}
+                    <Suspense fallback={<div style={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}><LoadingOutlined style={{ fontSize: '3rem' }} /></div>}>
+                        <Routes>
+                            {contentRoute.map((content) =>
+                                content.element ? (<Route
+                                    path={content.path}
+                                    element={content.element}
+                                    key={content.id}
+                                    index={content.index} />
+                                ) : null
+                            )}
 
 
-                    </Routes>
+                        </Routes>
+                    </Suspense>
                 </div>
             </Content>
             <Footer
