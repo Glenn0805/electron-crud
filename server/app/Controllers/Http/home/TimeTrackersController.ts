a8932a5eb4e0b01401e5f0886a9894b32079ec69
Glen0805 <cavitaglenn22131@gmail.com>
2022-11-04 08:21:11 +0800

    added function for getting all tasks

--- server/app/Controllers/Http/home/TimeTrackersController.ts
+++ server/app/Controllers/Http/home/TimeTrackersController.ts
@@ -1,8 +1,19 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Task from 'App/Models/Task'
 
 export default class TimeTrackersController {
     public async testFunction({response}:HttpContextContract){
 
         return response.send({test:'HELLO'})
+    }
+
+    public async getAllTasks({response}:HttpContextContract){
+        try {
+            const taskList = await Task.query()
+
+            return response.send({taskList})
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
--- server/routes/home/time-tracker.ts
+++ server/routes/home/time-tracker.ts
@@ -4,5 +4,6 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(()=>{
     Route.get('/','TimeTrackersController.testFunction')
+    Route.get('/get-task','TimeTrackersController.getAllTasks')
 }).prefix('home')
 .namespace('App/Controllers/Http/home')
