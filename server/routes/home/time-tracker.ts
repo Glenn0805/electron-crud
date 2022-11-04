e58e08d05e8b15c373bb313c47151d892ccb13fd
Glen0805 <cavitaglenn22131@gmail.com>
2022-11-04 09:23:30 +0800

    add route for adding task

--- server/routes/home/time-tracker.ts
+++ server/routes/home/time-tracker.ts
@@ -5,5 +5,7 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(()=>{
     Route.get('/','TimeTrackersController.testFunction')
     Route.get('/get-task','TimeTrackersController.getAllTasks')
+
+    Route.post('/add-task','TimeTrackersController.addTask')
 }).prefix('home')
 .namespace('App/Controllers/Http/home')
